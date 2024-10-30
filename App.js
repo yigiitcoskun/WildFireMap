da67e005b48d6d87837291f51c397ef06286b7b3
Yiğit Coşkun <70442363+yigiitcoskun@users.noreply.github.com>
2024-10-30 10:55:27 +0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Marker } from "react-native-maps";
+import MapView from "react-native-map-clustering";
+import axios from 'axios';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+        .then((response) => {
+          setData(response.data.events);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        {loading ? (
+            <View style={{ justifyContent: 'center', flex: 1, alignItems: 'center' }}>
+              <Text>Yükleniyor...</Text>
+            </View>
+        ) : (
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                  latitude: 36.3719444,
+                  longitude: -97.5683333,
+                  latitudeDelta: 10.0922,
+                  longitudeDelta: 10.0421,
+                }}
+            >
+              {data.map((item, index) => (
+                  item.categories[0].id == '8' && item.geometries.map((geometry, geoIndex) => (
+                      <Marker
+                          key={`${index}-${geoIndex}`}
+                          coordinate={{
+                            latitude: geometry.coordinates[1],
+                            longitude: geometry.coordinates[0],
+                          }}
+                          title={item.title}
+                          description={item.categories[0].title}
+                      />
+                  ))
+              ))}
+            </MapView>
+        )}
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
@@ -17,5 +62,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
